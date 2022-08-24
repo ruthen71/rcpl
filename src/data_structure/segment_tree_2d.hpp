5b6d47567e5183988aa84c124aa3d5d3ad912855
ruthen71 <takahero2016@gmail.com>
2022-08-24 13:29:07 +0900

    Add Segment Tree 2D

---
+++ src/data_structure/segment_tree_2d.hpp
@@ -0,0 +1,105 @@
+#pragma once
+
+template <class Monoid> struct segment_tree_2d {
+    using S = typename Monoid::value_type;
+    int _h, logh, sizeh, _w, logw, sizew;
+    std::vector<std::vector<S>> d;
+    segment_tree_2d() : segment_tree_2d(0, 0) {}
+    segment_tree_2d(int h, int w) : segment_tree_2d(std::vector<std::vector<S>>(h, std::vector<S>(w, Monoid::e()))) {}
+    segment_tree_2d(const std::vector<std::vector<S>>& v) : _h((int)v.size()), _w((int)v[0].size()) {
+        logh = 0;
+        while ((1U << logh) < (unsigned int)(_h)) logh++;
+        sizeh = 1 << logh;
+        logw = 0;
+        while ((1U << logw) < (unsigned int)(_w)) logw++;
+        sizew = 1 << logw;
+        d = std::vector<std::vector<S>>(2 * sizeh, std::vector<S>(2 * sizew, Monoid::e()));
+        d.shrink_to_fit();  // MLE 対策
+        for (int i = 0; i < _h; i++) {
+            for (int j = 0; j < _w; j++) {
+                d[i + sizeh][j + sizew] = v[i][j];
+            }
+        }
+        for (int i = sizeh - 1; i >= 1; i--) {
+            for (int j = sizew; j < 2 * sizew; j++) {
+                update_bottom(i, j);
+            }
+        }
+        for (int i = 0; i < 2 * sizeh; i++) {
+            for (int j = sizew - 1; j >= 1; j--) {
+                update_else(i, j);
+            }
+        }
+    }
+
+    void update_bottom(int i, int j) { d[i][j] = Monoid::op(d[i << 1][j], d[(i << 1) | 1][j]); }
+
+    void update_else(int i, int j) { d[i][j] = Monoid::op(d[i][j << 1], d[i][(j << 1) | 1]); }
+
+    void set(int h, int w, const S& x) {
+        assert(0 <= h and h < _h and 0 <= w and w < _w);
+        h += sizeh;
+        w += sizew;
+        d[h][w] = x;
+        for (int i = 1; i <= logh; i++) update_bottom(h >> i, w);
+        for (int i = 0; i <= logh; i++) {
+            for (int j = 1; j <= logw; j++) {
+                update_else(h >> i, w >> j);
+            }
+        }
+    }
+
+    void chset(int h, int w, const S& x) {
+        assert(0 <= h and h < _h and 0 <= w and w < _w);
+        h += sizeh;
+        w += sizew;
+        d[h][w] = Monoid::op(d[h][w], x);
+        for (int i = 1; i <= logh; i++) update_bottom(h >> i, w);
+        for (int i = 0; i <= logh; i++) {
+            for (int j = 1; j <= logw; j++) {
+                update_else(h >> i, w >> j);
+            }
+        }
+    }
+
+    S operator()(int h, int w) const {
+        assert(0 <= h and h < _h and 0 <= w and w < _w);
+        return d[h + sizeh][w + sizew];
+    }
+
+    S inner_prod(int h, int w1, int w2) {
+        S sml = Monoid::e(), smr = Monoid::e();
+        while (w1 < w2) {
+            if (w1 & 1) sml = Monoid::op(sml, d[h][w1++]);
+            if (w2 & 1) smr = Monoid::op(d[h][--w2], smr);
+            w1 >>= 1;
+            w2 >>= 1;
+        }
+        return Monoid::op(sml, smr);
+    }
+
+    S prod(int h1, int w1, int h2, int w2) {
+        assert(0 <= h1 and h1 <= h2 and h2 <= _h);
+        assert(0 <= w1 and w1 <= w2 and w2 <= _w);
+        S sml = Monoid::e(), smr = Monoid::e();
+        h1 += sizeh;
+        h2 += sizeh;
+        w1 += sizew;
+        w2 += sizew;
+
+        while (h1 < h2) {
+            if (h1 & 1) sml = Monoid::op(sml, inner_prod(h1++, w1, w2));
+            if (h2 & 1) smr = Monoid::op(inner_prod(--h2, w1, w2), smr);
+            h1 >>= 1;
+            h2 >>= 1;
+        }
+        return Monoid::op(sml, smr);
+    }
+
+    S all_prod() const { return d[1][1]; }
+};
+
+/**
+ * @brief 2D Segment Tree (2次元セグメント木)
+ * @docs docs/data_structure/segment_tree_2d.md
+ */
---
+++ verify/aoj_other/aoj_1068.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1068"
+
+#include <bits/stdc++.h>
+
+#include "src/algebra/monoid_min.hpp"
+#include "src/data_structure/segment_tree_2d.hpp"
+
+void solve(int R, int C, int Q) {
+    std::vector<std::vector<int>> A(R, std::vector<int>(C));
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            std::cin >> A[i][j];
+        }
+    }
+    segment_tree_2d<monoid_min<int>> seg(A);
+    while (Q--) {
+        int r1, c1, r2, c2;
+        std::cin >> r1 >> c1 >> r2 >> c2;
+        std::cout << seg.prod(r1, c1, r2 + 1, c2 + 1) << '\n';
+    }
+    return;
+}
+
+int main() {
+    int R, C, Q;
+    while (std::cin >> R >> C >> Q, !(R == 0 and C == 0 and Q == 0)) solve(R, C, Q);
+    return 0;
+}
