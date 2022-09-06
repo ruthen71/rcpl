b98bcccb22df01dd51e7bc63b6331bca64b8f456
ruthen71 <takahero2016@gmail.com>
2022-09-06 17:35:35 +0900

    Add osa_k

---
+++ src/math/osa_k.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+struct osa_k {
+    int n;
+    std::vector<int> min_factor;
+
+    osa_k(int n) : n(n), min_factor(n + 1) {
+        std::iota(min_factor.begin(), min_factor.end(), 0);
+        if (n >= 0) min_factor[0] = -1;
+        if (n >= 1) min_factor[1] = -1;
+        for (int p = 2; p * p <= n; p++) {
+            if (min_factor[p] != p) continue;
+            for (int i = p * p; i <= n; i += p) {
+                if (min_factor[i] != i) continue;
+                min_factor[i] = p;
+            }
+        }
+    }
+
+    bool is_prime(int x) {
+        assert(x <= n);
+        return min_factor[x] == x;
+    }
+
+    std::map<int, int> prime_factor(int x) {
+        assert(x <= n);
+        std::map<int, int> pf;
+        while (x > 1) {
+            pf[min_factor[x]]++;
+            x /= min_factor[x];
+        }
+        return pf;
+    }
+};
---
+++ verify/aoj_alds1/aoj_alds1_1_c_osa_k.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_1_C"
+
+#include <bits/stdc++.h>
+
+#include "src/math/osa_k.hpp"
+
+int main() {
+    auto osak = osa_k(100000000);
+    int n;
+    std::cin >> n;
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        int a;
+        std::cin >> a;
+        ans += osak.is_prime(a);
+    }
+    std::cout << ans << '\n';
+    return 0;
+}
