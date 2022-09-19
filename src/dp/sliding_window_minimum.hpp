5d4111b5aa326e719031cc9ada0e127f9d24c340
ruthen71 <takahero2016@gmail.com>
2022-09-19 23:16:33 +0900

    Add sliding window minimum

---
+++ src/dp/sliding_window_minimum.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+template <class T> std::vector<T> sliding_window_minimum(const std::vector<T> &a, const int k) {
+    int n = (int)a.size();
+    std::vector<T> res(n - k + 1);
+    std::deque<T> deq;
+    for (int i = 0; i < n; i++) {
+        while (!deq.empty() and a[deq.back()] >= a[i]) deq.pop_back();
+        deq.push_back(i);
+        if (i - k + 1 >= 0) {
+            res[i - k + 1] = a[deq.front()];
+            if (deq.front() == i - k + 1) deq.pop_front();
+        }
+    }
+    return res;
+}
+
+/**
+ * @brief Sliding Window Minimum (スライド最小値)
+ * @docs docs/dp/sliding_window_minimum.md
+ */
---
+++ verify/aoj_dsl/aoj_dsl_3_d.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_3_D"
+
+#include <bits/stdc++.h>
+
+#include "src/dp/sliding_window_minimum.hpp"
+
+int main() {
+    int N, L;
+    std::cin >> N >> L;
+    std::vector<int> a(N);
+    for (int i = 0; i < N; i++) std::cin >> a[i];
+    auto b = sliding_window_minimum(a, L);
+    for (int i = 0; i < N - L + 1; i++) std::cout << b[i] << " \n"[i == N - L];
+    return 0;
+}
