f7139a473a5cb8252deaae8fcbbeeb897ebde0db
ruthen71 <takahero2016@gmail.com>
2022-09-23 05:31:03 +0900

    Update monoid_min/max_index

--- src/algebra/monoid_max_index.hpp
+++ src/algebra/monoid_max_index.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 template <class T, bool left = true> struct monoid_max_index {
-    using value_type = pair<T, int>;
-    using S = value_type;
+    using S = std::pair<T, int>;
+    using value_type = S;
     static constexpr S op(S a, S b) {
         if (a.first > b.first) return a;
         if (a.first < b.first) return b;
