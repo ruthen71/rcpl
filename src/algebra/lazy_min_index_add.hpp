4017aeb9fc2eaa527832846c302d0f13edb37b6c
ruthen71 <takahero2016@gmail.com>
2022-09-23 16:03:00 +0900

    Add lazy max/min index add/set

---
+++ src/algebra/lazy_min_index_add.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+template <class T, bool left = true> struct lazy_min_index_add {
+    using S = std::pair<T, int>;
+    using F = T;
+    using value_type_S = S;
+    using value_type_F = F;
+    static constexpr S op(S a, S b) {
+        if (a.first < b.first) return a;
+        if (a.first > b.first) return b;
+        if (a.second > b.second) swap(a, b);
+        return (left ? a : b);
+    }
+    static constexpr S e() { return {std::numeric_limits<T>::max(), -1}; }
+    static constexpr S mapping(F f, S x) { return {f + x.first, x.second}; }
+    static constexpr F composition(F f, F g) { return f + g; }
+    static constexpr F id() { return 0; }
+};
