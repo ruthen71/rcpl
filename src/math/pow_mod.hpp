d261f94720a6189ee7b4114d453207af57d42a87
ruthen71 <takahero2016@gmail.com>
2022-09-25 07:00:30 +0900

    Fix bug (a < 0, 0^0)

--- src/math/pow_mod.hpp
+++ src/math/pow_mod.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
-long long pow_mod(long long a, long long n, long long mod) {
+long long pow_mod(long long a, long long n, const long long mod) {
+    assert(n >= 0 and mod >= 0);
     a %= mod;
+    if (a < 0) a += mod;
+    if (a == 0) return (n == 0 ? 1 % mod : 0);  // 0 ^ 0 = 1
     long long res = 1;
     while (n) {
         if (n & 1) res = res * a % mod;
