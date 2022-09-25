be4fba8a73b2276928118e154500a8b9c2ed08e8
ruthen71 <takahero2016@gmail.com>
2022-09-26 00:30:57 +0900

    Update pow_mod (remove %)

--- src/math/pow_mod.hpp
+++ src/math/pow_mod.hpp
@@ -4,7 +4,7 @@ long long pow_mod(long long a, long long n, const long long mod) {
     assert(n >= 0 and mod >= 0);
     a %= mod;
     if (a < 0) a += mod;
-    if (a == 0) return (n == 0 ? 1 % mod : 0);  // 0 ^ 0 = 1
+    if (a == 0) return (n == 0 ? (mod != 1 ? 1 : 0) : 0);  // 0 ^ 0 = 1
     long long res = 1;
     while (n) {
         if (n & 1) res = res * a % mod;
