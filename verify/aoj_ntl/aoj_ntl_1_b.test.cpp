143909ccb4fe0ce743ac44c37d3c40938310c6e7
ruthen71 <takahero2016@gmail.com>
2022-09-26 02:31:08 +0900

    Update pow_mod test

--- verify/aoj_ntl/aoj_ntl_1_b.test.cpp
+++ verify/aoj_ntl/aoj_ntl_1_b.test.cpp
@@ -5,6 +5,23 @@
 #include "src/math/pow_mod.hpp"
 
 int main() {
+    // a = 0
+    assert(pow_mod(0, 0, 1) == 0);  // 0 ^ 0 = 1
+    assert(pow_mod(0, 0, 2) == 1);
+    assert(pow_mod(0, 0, 3) == 1);
+    assert(pow_mod(0, 1, 1) == 0);  // 0 ^ 1 = 0
+    assert(pow_mod(0, 1, 2) == 0);
+    assert(pow_mod(0, 1, 3) == 0);
+    assert(pow_mod(0, 2, 1) == 0);  // 0 ^ 2 = 0
+    assert(pow_mod(0, 2, 2) == 0);
+    assert(pow_mod(0, 2, 3) == 0);
+    // a > 0, n = 0
+    assert(pow_mod(1, 0, 1) == 0);  // 1 ^ 0 = 1
+    assert(pow_mod(1, 0, 2) == 1);
+    assert(pow_mod(1, 0, 3) == 1);
+    assert(pow_mod(2, 0, 1) == 0);  // 2 ^ 0 = 1
+    assert(pow_mod(2, 0, 2) == 1);
+    assert(pow_mod(2, 0, 3) == 1);
     long long m, n;
     std::cin >> m >> n;
     std::cout << pow_mod(m, n, 1000000007) << '\n';
