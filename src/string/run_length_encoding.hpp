bdabd126040202205d0a5db41caf4c5240cb68cc
ruthen71 <takahero2016@gmail.com>
2022-11-11 00:45:00 +0900

    Add RLE

---
+++ src/string/run_length_encoding.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+std::vector<std::pair<char, int>> run_length_encoding(std::string &S) {
+    assert(S.size() > 0);
+    std::vector<std::pair<char, int>> res = {{S.front(), 0}};
+    for (auto &si : S) {
+        if (si != res.back().first) {
+            res.push_back({si, 0});
+        }
+        res.back().second++;
+    }
+    return res;
+}
+
+/**
+ * @brief Run Length Encoding (ランレングス圧縮)
+ */
