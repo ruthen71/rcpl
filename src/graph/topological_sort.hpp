caa5ca125f9bada0116ff90cc2a840ad2d26bb6c
ruthen71 <takahero2016@gmail.com>
2022-11-13 00:39:21 +0900

    Add topological sort

---
+++ src/graph/topological_sort.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "src/graph/template.hpp"
+
+template <class Graph> std::vector<int> topological_sort(Graph &G) {
+    assert(G.is_directed());
+    int N = (int)G.size();
+    std::vector<int> indeg(N, 0);
+    for (int i = 0; i < N; i++) {
+        for (auto &e : G[i]) indeg[e.to]++;
+    }
+    std::vector<int> res;
+    for (int i = 0; i < N; i++) {
+        if (indeg[i] == 0) res.push_back(i);
+    }
+    int i = 0;
+    while (i < N) {
+        int v = res[i];
+        i++;
+        for (auto &e : G[v]) {
+            indeg[e.to]--;
+            if (indeg[e.to] == 0) res.push_back(e.to);
+        }
+    }
+    if ((int)res.size() != N) {
+        return std::vector<int>();
+    }
+    return res;
+}
+
+/**
+ * @brief Topological Sort (トポロジカルソート)
+ * @docs docs/graph/topological_sort.md
+ */
---
+++ verify/aoj_other/aoj_0519.test.cpp
@@ -0,0 +1,27 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0519"
+
+#include <bits/stdc++.h>
+
+#include "src/graph/topological_sort.hpp"
+
+int main() {
+    int N, M;
+    std::cin >> N >> M;
+    std::vector<int> a(M), b(M);
+    std::vector<std::vector<int>> C(N, std::vector<int>(N));
+    graph<int, true> G(N);
+    for (int i = 0; i < M; i++) {
+        std::cin >> a[i] >> b[i];
+        a[i]--, b[i]--;
+        G.add_edge(a[i], b[i]);
+        C[a[i]][b[i]] = 1;
+    }
+    auto res = topological_sort(G);
+    for (auto &e : res) std::cout << e + 1 << '\n';
+    int ok = 1;
+    for (int i = 0; i < N - 1; i++) {
+        ok &= C[res[i]][res[i + 1]];
+    }
+    std::cout << 1 - ok << '\n';
+    return 0;
+}
