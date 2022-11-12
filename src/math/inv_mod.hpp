a385e3059d504c6ba7014a82f279a33b48d30562
ruthen71 <takahero2016@gmail.com>
2022-11-12 23:35:04 +0900

    Update inv_mod

--- src/math/inv_mod.hpp
+++ src/math/inv_mod.hpp
@@ -3,6 +3,8 @@
 #include "src/math/ext_gcd.hpp"
 
 long long inv_mod(long long a, long long mod) {
+    a %= mod;
+    if (a < 0) a += mod;
     assert(a != 0);
     long long x, y;
     ext_gcd(a, mod, x, y);
