6e1d5aa1b435ca0a0df202fd3ad35431f2b51588
ruthen71 <takahero2016@gmail.com>
2023-01-16 01:16:58 +0900

    Update binomial (add extend())

--- src/math/binomial.hpp
+++ src/math/binomial.hpp
@@ -1,23 +1,44 @@
 #pragma once
 
 template <class Mint> struct binomial {
-    std::vector<Mint> fact, finv;
+    std::vector<Mint> f, g;
 
-    binomial(int N) : fact(N + 1), finv(N + 1) {
-        fact[0] = 1;
-        for (int i = 1; i <= N; i++) fact[i] = fact[i - 1] * i;
-        finv[N] = 1 / fact[N];
-        for (int i = N - 1; i >= 0; i--) finv[i] = finv[i + 1] * (i + 1);
+    binomial(int N = 0) {
+        f.resize(1, Mint(1));
+        g.resize(1, Mint(1));
+        while (N >= (int)f.size()) extend();
+    }
+
+    void extend() {
+        int n = (int)f.size();
+        int m = n * 2;
+        f.resize(m);
+        g.resize(m);
+        for (int i = n; i < m; i++) f[i] = f[i - 1] * Mint(i);
+        g[m - 1] = 1 / f[m - 1];
+        for (int i = m - 2; i >= n; i--) g[i] = g[i + 1] * Mint(i + 1);
+    }
+
+    Mint fact(int i) {
+        if (i < 0) return Mint(0);
+        while (i >= (int)f.size()) extend();
+        return f[i];
+    }
+
+    Mint finv(int i) {
+        if (i < 0) return Mint(0);
+        while (i >= (int)g.size()) extend();
+        return g[i];
     }
 
     Mint C(int N, int K) {
         if (N < 0 || K < 0 || N < K) return 0;
-        return fact[N] * finv[N - K] * finv[K];
+        return fact(N) * finv(N - K) * finv(K);
     }
 
     Mint P(int N, int K) {
         if (N < 0 || K < 0 || N < K) return 0;
-        return fact[N] * finv[N - K];
+        return fact(N) * finv(N - K);
     }
 
     Mint C_naive(int N, int K) {
