384ba181dfeb73ea355a50c28c6aa542817f6173
ruthen71 <takahero2016@gmail.com>
2023-02-11 14:20:38 +0900

    Add ccw

---
+++ geometry/ccw.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "geometry/point.hpp"
+
+// counter clockwise
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=CGL_1_C
+constexpr int COUNTER_CLOCKWISE = 1;  // a-b-c counter clockwise
+constexpr int CLOCKWISE = -1;         // a-b-c clockwise
+constexpr int ONLINE_BACK = 2;        // c-a-b line
+constexpr int ONLINE_FRONT = -2;      // a-b-c line
+constexpr int ON_SEGMENT = 0;         // a-c-b line
+int ccw(const Point &a, Point b, Point c) {
+    b = b - a, c = c - a;
+    if (sign(cross(b, c)) == 1) return COUNTER_CLOCKWISE;
+    if (sign(cross(b, c)) == -1) return CLOCKWISE;
+    if (sign(dot(b, c)) == -1) return ONLINE_BACK;
+    if (norm(b) < norm(c)) return ONLINE_FRONT;
+    return ON_SEGMENT;
+}
---
+++ verify/aoj_cgl/aoj_cgl_1_c.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=CGL_1_C"
+
+#include <bits/stdc++.h>
+
+#include "geometry/ccw.hpp"
+
+int main() {
+    Point P1, P2;
+    std::cin >> P1 >> P2;
+    int Q;
+    std::cin >> Q;
+    while (Q--) {
+        Point P3;
+        std::cin >> P3;
+        auto ans = ccw(P1, P2, P3);
+        if (ans == COUNTER_CLOCKWISE) {
+            std::cout << "COUNTER_CLOCKWISE\n";
+        } else if (ans == CLOCKWISE) {
+            std::cout << "CLOCKWISE\n";
+        } else if (ans == ONLINE_BACK) {
+            std::cout << "ONLINE_BACK\n";
+        } else if (ans == ONLINE_FRONT) {
+            std::cout << "ONLINE_FRONT\n";
+        } else {
+            std::cout << "ON_SEGMENT\n";
+        }
+    }
+    return 0;
+}
