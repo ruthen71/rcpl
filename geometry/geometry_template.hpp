010ffccda6a26651f1bf09494f108148add633a2
ruthen71 <takahero2016@gmail.com>
2023-02-11 12:18:28 +0900

    Add geometry template

---
+++ geometry/geometry_template.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+// template
+using Double = double;
+const Double EPS = 1e-8;
+const Double PI = std::acos(-1);
+inline int sign(const Double &x) { return x <= -EPS ? -1 : (x >= EPS ? 1 : 0); }
+inline bool equal(const Double &a, const Double &b) { return sign(a - b) == 0; }
+Double radian_to_degree(const Double &r) { return r * 180.0 / PI; }
+Double degree_to_radian(const Double &d) { return d * PI / 180.0; }
