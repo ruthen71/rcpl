1c760bf2d4bd6205e86deeeda1599047c580d981
ruthen71 <takahero2016@gmail.com>
2023-02-16 23:51:50 +0900

    Add all file

---
+++ geometry/all.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "geometry/geometry_template.hpp"
+#include "geometry/point.hpp"
+#include "geometry/line.hpp"
+#include "geometry/segment.hpp"
+#include "geometry/circle.hpp"
+#include "geometry/projection.hpp"
+#include "geometry/reflection.hpp"
+#include "geometry/ccw.hpp"
+#include "geometry/is_orthogonal.hpp"
+#include "geometry/is_parallel.hpp"
