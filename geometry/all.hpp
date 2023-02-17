d52382c9435894c985fc2b5e6596a0e7454dc89e
ruthen71 <takahero2016@gmail.com>
2023-02-17 09:50:28 +0900

    update all and misc

--- geometry/all.hpp
+++ geometry/all.hpp
@@ -1,12 +1,24 @@
 #pragma once
 #include "geometry/geometry_template.hpp"
+
 #include "geometry/point.hpp"
 #include "geometry/line.hpp"
 #include "geometry/segment.hpp"
 #include "geometry/circle.hpp"
+
 #include "geometry/projection.hpp"
 #include "geometry/reflection.hpp"
 #include "geometry/ccw.hpp"
 #include "geometry/is_orthogonal.hpp"
 #include "geometry/is_parallel.hpp"
+
+#include "geometry/is_intersect_ll.hpp"
+#include "geometry/is_intersect_lp.hpp"
 #include "geometry/is_intersect_ss.hpp"
+#include "geometry/is_intersect_sp.hpp"
+#include "geometry/tangent_number_cc.hpp"
+#include "geometry/is_intersect_cc.hpp"
+#include "geometry/is_intersect_cp.hpp"
+
+#include "geometry/cross_point_ll.hpp"
+#include "geometry/cross_point_ss.hpp"
--- geometry/cross_point_ll.hpp
+++ geometry/cross_point_ll.hpp
@@ -7,7 +7,6 @@ Point cross_point_ll(const Line &l1, const Line &l2) {
     Point base = l1.b - l1.a;
     Double d12 = cross(base, l2.b - l2.a);
     Double d1 = cross(base, l1.b - l2.a);
-    // 一致(sign(d12) == 0 は is_parallelと本質的に同じ, sign(d1) == 0は重なっているか)
     if (sign(d12) == 0) {
         // parallel
         if (sign(d1) == 0) {
