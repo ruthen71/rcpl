577dc35448c9eba82af1bed3f5ce47fe571e8db8
ruthen71 <takahero2016@gmail.com>
2023-02-17 17:27:32 +0900

    Add cross point (circle and circle)

---
+++ geometry/cross_point_cc.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "geometry/is_intersect_cc.hpp"
+
+// cross point (circle and circle)
+// // http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=CGL_7_E
+std::vector<Point> cross_point_cc(const Circle &c1, const Circle &c2) {
+    if (!is_intersect_cc(c1, c2)) return {};
+    Double d = std::abs(c1.o - c2.o);
+    Double a = acos((std::norm(c1.r) - std::norm(c2.r) + std::norm(d)) / (2 * c1.r * d));
+    Double t = std::arg(c2.o - c1.o);
+    Point p = c1.o + std::polar(c1.r, t + a);
+    Point q = c1.o + std::polar(c1.r, t - a);
+    if (equal(p.real(), q.real()) and equal(p.imag(), q.imag())) return {p};
+    return {p, q};
+}
---
+++ verify/aoj_cgl/aoj_cgl_7_e.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=CGL_7_E"
+
+#include <bits/stdc++.h>
+
+#include "geometry/cross_point_cc.hpp"
+
+int main() {
+    Circle C1, C2;
+    std::cin >> C1 >> C2;
+    auto res = cross_point_cc(C1, C2);
+    std::sort(res.begin(), res.end());
+    if (res.size() == 1) res.push_back(res[0]);
+    std::cout << res[0] << ' ' << res[1] << '\n';
+    return 0;
+}
