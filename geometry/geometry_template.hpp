083365ff7a6c2394d774aaef676e864751db85d8
ruthen71 <takahero2016@gmail.com>
2023-03-05 01:44:59 +0900

    EPS

--- geometry/geometry_template.hpp
+++ geometry/geometry_template.hpp
@@ -2,7 +2,7 @@
 
 // template
 using Double = double;
-const Double EPS = 1e-10;
+const Double EPS = 1e-8;
 const Double PI = std::acos(Double(-1));
 inline int sign(const Double &x) { return x <= -EPS ? -1 : (x >= EPS ? 1 : 0); }
 inline bool equal(const Double &a, const Double &b) { return sign(a - b) == 0; }
