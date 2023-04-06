5b01390158e92d4cd8f968863f8412ed318a1197
ruthen71 <takahero2016@gmail.com>
2023-04-06 10:02:29 +0900

    acos は引数が constexpr でも constexpr にならない

--- geometry/point.hpp
+++ geometry/point.hpp
@@ -3,7 +3,7 @@
 // point
 template <typename T> struct Point {
     static T EPS;
-    static constexpr T PI = std::acos(T(-1));
+    static constexpr T PI = 3.1415926535'8979323846'2643383279'5028841971;
     static void set_eps(const T &e) { EPS = e; }
     T x, y;
     Point(const T x = T(0), const T y = T(0)) : x(x), y(y) {}
