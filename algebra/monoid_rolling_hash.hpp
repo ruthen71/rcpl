1fcfcf1ad6fbb34d214e9d9f4b3f50906438142b
ruthen71 <takahero2016@gmail.com>
2023-04-16 23:03:31 +0900

    Update monoid rolling hash

--- algebra/monoid_rolling_hash.hpp
+++ algebra/monoid_rolling_hash.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
-#include "string/rolling_hash.hpp"
-
-RollingHash<ModInt261> rh;
-
 template <class Mint> struct MonoidRollingHash {
-    using S = std::pair<Mint, int>;
+    using S = std::pair<Mint, Mint>;  // {hash(s), base ^ len(s)}
     using value_type = S;
-    static constexpr S op(S a, S b) { return {rh.combine(a.first, b.first, b.second), a.second + b.second}; }
-    static constexpr S e() { return {0, 0}; }
+
+    static Mint base;
+
+    static void constexpr set_base(Mint b = Mint(0)) {
+        if (b == Mint(0)) {
+            std::mt19937_64 mt(std::chrono::steady_clock::now().time_since_epoch().count());
+            std::uniform_int_distribution<uint64_t> rand(1, Mint::mod() - 1);
+            base = Mint(rand(mt));
+        } else {
+            base = b;
+        }
+    }
+
+    static constexpr S make_element(Mint x) { return make_pair(x, base); }
+
+    static constexpr S op(S a, S b) { return {a.first * b.second + b.first, a.second * b.second}; }
+    static constexpr S e() { return {0, 1}; }  // {hash(""), base ^ len("")}
 };
--- docs/algebra/monoid_rolling_hash.md
+++ docs/algebra/monoid_rolling_hash.md
@@ -3,8 +3,33 @@ title: ローリングハッシュ
 documentation_of: //algebra/monoid_rolling_hash.hpp
 ---
 
+## 使用方法
+
+```cpp
+// 既に mint が定義されている必要がある
+#include "algebra/monoid_rolling_hash.hpp"
+using mrh = MonoidRollingHash<mint>;
+template <> mint mrh::base = 0;
+
+int main() {
+    mrh::set_base();
+    // code here
+}
+```
+
+`mrh::set_base(x)` とすると基数に `x` にセットできる
+
+モノイドを作成するには `mrh::make_element(x)` とする
+
+```cpp
+vector<pair<mint, mint>> seginit;
+seginit.push_back(mrh::make_element(x));
+SegmentTree<mrh> seg(seginit);
+```
+
 ## 使用例
 
 - [ABC285 F](https://atcoder.jp/contests/abc285/tasks/abc285_f)
 - 2つ以上のmodを組み合わせるなら、include ではなくべた書きするしかない
     - が、基数が $ 2^{61} - 1 $ なので余程のことがなければ衝突しないと思われる
+- [ABC298 D](https://atcoder.jp/contests/abc298/submissions/40706415)
