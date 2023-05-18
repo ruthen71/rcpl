331b9f48fa56b18576e3843b6f96275c74fb76a3
ruthen71 <takahero2016@gmail.com>
2023-05-18 15:52:01 +0900

    Update mat

--- icpc/matrix.hpp
+++ icpc/matrix.hpp
@@ -4,16 +4,17 @@
 
 constexpr ll MOD = 998244353;
 
-V<V<ll>> mul(V<V<ll>> a, V<V<ll>> b) {
+using Mat = V<V<ll>>;
+Mat mul(Mat& a, Mat& b) {
     int N = int(a.size());
-    V<V<ll>> c(N, V<ll>(N));
+    Mat c(N, V<ll>(N));
     REP(i, N) REP(k, N) REP(j, N) c[i][j] = (c[i][j] + a[i][k] * b[k][j] % MOD) % MOD;
     return c;
 }
 
-V<V<ll>> pow(V<V<ll>> a, ll k) {
+Mat pow(Mat& a, ll k) {
     int N = int(a.size());
-    V<V<ll>> res(N, V<ll>(N));
+    Mat res(N, V<ll>(N));
     REP(i, N) res[i][i] = 1;
     while (k) {
         if (k & 1) res = mul(res, a);
