4f584e0a8c6288a7f221c0bd0264164c2b22b95c
ruthen71 <takahero2016@gmail.com>
2024-01-15 16:49:41 +0900

    Update uf

--- data_structure/unionfind.hpp
+++ data_structure/unionfind.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
 struct UnionFind {
+    int n;
     std::vector<int> parents;
 
     UnionFind() {}
-    UnionFind(int n) : parents(n, -1) {}
+    UnionFind(int n) : n(n), parents(n, -1) {}
 
     int leader(int x) { return parents[x] < 0 ? x : parents[x] = leader(parents[x]); }
 
@@ -20,6 +21,23 @@ struct UnionFind {
     bool same(int x, int y) { return leader(x) == leader(y); }
 
     int size(int x) { return -parents[leader(x)]; }
+
+    std::vector<std::vector<int>> groups() {
+        std::vector<int> leader_buf(n), group_size(n);
+        for (int i = 0; i < n; i++) {
+            leader_buf[i] = leader(i);
+            group_size[leader_buf[i]]++;
+        }
+        std::vector<std::vector<int>> result(n);
+        for (int i = 0; i < n; i++) {
+            result[i].reserve(group_size[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            result[leader_buf[i]].push_back(i);
+        }
+        result.erase(std::remove_if(result.begin(), result.end(), [&](const std::vector<int>& v) { return v.empty(); }), result.end());
+        return result;
+    }
 
     void init(int n) { parents.assign(n, -1); }  // reset
 };
