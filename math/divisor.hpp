4f37b0737c574c1ad8fcdd52e56b1a818d9aa876
ruthen71 <takahero2016@gmail.com>
2024-05-28 13:48:58 +0900

    Update divisor

--- math/divisor.hpp
+++ math/divisor.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-std::vector<long long> divisor(long long n) {
-    std::vector<long long> res;
-    for (long long i = 1; i * i <= n; i++) {
+template <class T> std::vector<T> divisor(const T n) {
+    std::vector<T> res;
+    for (T i = T(1); i * i <= n; i++) {
         if (n % i == 0) {
             res.push_back(i);
             if (i * i != n) res.push_back(n / i);
