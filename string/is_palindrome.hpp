b82b7b3f731aa37bd1c5ee8088ef6ae72272df26
ruthen71 <takahero2016@gmail.com>
2024-07-21 14:20:25 +0900

    Add is_palindrome

---
+++ string/is_palindrome.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+template <class T> bool is_palindrome(const std::vector<T>& s) {
+    const int n = (int)(s.size());
+    bool res = true;
+    for (int i = 0; i < n / 2; i++) res &= s[i] == s[n - 1 - i];
+    return res;
+}
+
+bool is_palindrome(const std::string& s) {
+    const int n = (int)(s.size());
+    std::vector<int> s2(n);
+    for (int i = 0; i < n; i++) s2[i] = s[i];
+    return is_palindrome(s2);
+}
