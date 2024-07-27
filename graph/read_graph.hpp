271f3c79bb12eb6a9a509828eb24e1675720e0f9
ruthen71 <takahero2016@gmail.com>
2024-07-28 03:23:44 +0900

    erase read_grid

--- graph/read_graph.hpp
+++ graph/read_graph.hpp
@@ -41,36 +41,3 @@ template <class T> Graph<T> read_parent(const int n, const bool weight = false, 
     }
     return g;
 }
-
-std::tuple<Graph<int>, std::vector<std::vector<int>>, std::vector<std::pair<int, int>>> read_grid(const int h, const int w, std::string rel = ".#") {
-    std::vector<std::string> s(h);
-    std::vector id(h, std::vector<int>(w, -1));
-    std::vector<std::pair<int, int>> loc;
-    int n = 0;
-    for (int i = 0; i < h; i++) {
-        std::cin >> s[i];
-        for (int j = 0; j < w; j++) {
-            if (s[i][j] == rel[1]) {
-                id[i][j] = n++;
-                loc.emplace_back(i, j);
-            }
-        }
-    }
-    int m = 0;
-    Graph<int> g(n);
-    for (int i = 0; i < h; i++) {
-        for (int j = 0; j < w; j++) {
-            if (s[i][j] == rel[1]) {
-                if (i + 1 < h and s[i + 1][j] == rel[1]) {
-                    g[id[i][j]].push_back(Edge(id[i][j], id[i + 1][j], 1, m));
-                    g[id[i + 1][j]].push_back(Edge(id[i + 1][j], id[i][j], 1, m++));
-                }
-                if (j + 1 < w and s[i][j + 1] == rel[1]) {
-                    g[id[i][j]].push_back(Edge(id[i][j], id[i][j + 1], 1, m));
-                    g[id[i][j + 1]].push_back(Edge(id[i][j + 1], id[i][j], 1, m++));
-                }
-            }
-        }
-    }
-    return {g, id, loc};
-}
