74d34eee9d7e39e35d1398a4f52f88ea3ccbfc92
ruthen71 <takahero2016@gmail.com>
2024-07-29 02:05:39 +0900

    update sptree

--- graph/shortest_path_tree.hpp
+++ graph/shortest_path_tree.hpp
@@ -6,12 +6,16 @@
 
 // https://atcoder.jp/contests/abc252/tasks/abc252_e
 // 最短経路木のうちの 1 つを返す
-template <class T> Graph<T> shortest_path_tree(Graph<T>& g, std::vector<int>& par) {
+template <class T> Graph<T> shortest_path_tree(Graph<T>& g, std::vector<T>& dist, std::vector<int>& par) {
     const int n = (int)(g.size());
     Graph<T> sptree(n, true);
     for (auto&& e : g.edges) {
-        if (par[e.to] == e.from) sptree.add_edge(e.from, e.to, e.cost, e.id);
-        if (par[e.from] == e.to) sptree.add_edge(e.to, e.from, e.cost, e.id);
+        if (par[e.to] == e.from and dist[e.from] + e.cost == dist[e.to]) {
+            sptree.add_edge(e.from, e.to, e.cost, e.id);
+        }
+        if (!g.is_directed and par[e.from] == e.to and dist[e.to] + e.cost == dist[e.from]) {
+            if (par[e.from] == e.to) sptree.add_edge(e.to, e.from, e.cost, e.id);
+        }
     }
     return sptree;
 }
