7068c7eb562fb78769ad69bde273eac0b15aa134
ruthen71 <takahero2016@gmail.com>
2024-07-29 00:13:13 +0900

    update auxiliary tree

--- graph/auxiliary_tree.hpp
+++ graph/auxiliary_tree.hpp
@@ -4,14 +4,13 @@
 #include "graph/lowest_common_ancestor.hpp"
 
 template <class T> struct AuxiliaryTree {
-    Graph<T> g;
     int n, root;
     std::vector<int> preorder, rank;
     std::vector<T> depth;
     LowestCommonAncestor<T> lca;
 
-    AuxiliaryTree(Graph<T>& g, int root = 0) : n((int)(g.size())), root(root), g(g), lca(g, root) {
-        // DFSして行きがけ順に頂点を並べる
+    AuxiliaryTree(Graph<T>& g, const int root = 0) : n((int)(g.size())), root(root), lca(g, root) {
+        // DFS して行きがけ順に頂点を並べる
         depth.assign(n, T(0));
         rank.resize(n);
         auto dfs = [&](auto f, int cur, int par) -> void {
@@ -29,23 +28,30 @@ template <class T> struct AuxiliaryTree {
     // (圧縮後のグラフ, グラフの頂点番号 -> 元のグラフの頂点番号 の対応表)
     std::pair<Graph<T>, std::vector<int>> get(std::vector<int> vs) {
         if (vs.empty()) return {};
+
         auto comp = [&](int i, int j) -> bool { return rank[i] < rank[j]; };
-        std::sort(begin(vs), end(vs), comp);
+        std::sort(vs.begin(), vs.end(), comp);
         for (int i = 0, vslen = (int)(vs.size()); i + 1 < vslen; i++) {
             vs.emplace_back(lca.lca(vs[i], vs[i + 1]));
         }
-        std::sort(begin(vs), end(vs), comp);
-        vs.erase(unique(begin(vs), end(vs)), end(vs));
-        Graph<T> aux(vs.size());
+        std::sort(vs.begin(), vs.end(), comp);
+        vs.erase(unique(vs.begin(), vs.end()), vs.end());
+
+        // Auxiliary Tree
+        Graph<T> aux(vs.size(), false);
         std::vector<int> rs;
         rs.push_back(0);
+
+        // i は新しい頂点番号, vs[i] はもとの頂点番号
+        // vs は Auxiliary Tree の行きがけ順になっているのでループが DFS になっている
         for (int i = 1; i < (int)(vs.size()); i++) {
+            // LCA まで遡ってから辺を追加する
             int l = lca.lca(vs[rs.back()], vs[i]);
             while (vs[rs.back()] != l) rs.pop_back();
-            aux[rs.back()].push_back(Edge(rs.back(), i, depth[vs[i]] - depth[vs[rs.back()]], i - 1));
-            aux[i].push_back(Edge(i, rs.back(), depth[vs[i]] - depth[vs[rs.back()]], i - 1));
+            aux.add_edge(rs.back(), i, depth[vs[i]] - depth[vs[rs.back()]]);
             rs.push_back(i);
         }
+        aux.build();
         return {aux, vs};
     }
 };
