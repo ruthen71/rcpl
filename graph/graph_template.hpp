7cc88aa2bacc11987ff9d5639e0485e477a9768e
ruthen71 <takahero2016@gmail.com>
2024-07-28 19:37:11 +0900

    Update graph_template (CSR)

--- graph/graph_template.hpp
+++ graph/graph_template.hpp
@@ -1,19 +1,94 @@
 #pragma once
 
 #include <vector>
+#include <cassert>
+
 template <class T> struct Edge {
     int from, to;
     T cost;
     int id;
 
     Edge() = default;
-    Edge(int from, int to, T cost = 1, int id = -1) : from(from), to(to), cost(cost), id(id) {}
+    Edge(const int from, const int to, const T cost = T(1), const int id = -1) : from(from), to(to), cost(cost), id(id) {}
 
-    friend std::ostream &operator<<(std::ostream &os, const Edge<T> &e) {
-        // output format: "{ id : from -> to, cost }"
-        return os << "{ " << e.id << " : " << e.from << " -> " << e.to << ", " << e.cost << " }";
+    friend std::ostream& operator<<(std::ostream& os, const Edge<T>& e) {
+        // output format: {id: cost(from, to) = cost}
+        return os << "{" << e.id << ": cost(" << e.from << ", " << e.to << ") = " << e.cost << "}";
     }
 };
-
 template <class T> using Edges = std::vector<Edge<T>>;
-template <class T> using Graph = std::vector<std::vector<Edge<T>>>;
+
+template <class T, bool directed = false> struct Graph {
+    struct EdgeIterators {
+       public:
+        using Iterator = typename std::vector<Edge<T>>::iterator;
+        EdgeIterators() = default;
+        EdgeIterators(const Iterator& begit, const Iterator& endit) : begit(begit), endit(endit) {}
+        Iterator begin() const { return begit; }
+        Iterator end() const { return endit; }
+        size_t size() const { return std::distance(begit, endit); }
+        Edge<T>& operator[](int i) const { return begit[i]; }
+
+       private:
+        Iterator begit, endit;
+    };
+
+    int n, m;
+    bool is_build, is_directed;
+    std::vector<Edge<T>> edges;
+    // CSR (Compressed Row Storage) 形式用
+    std::vector<int> start;
+    std::vector<Edge<T>> csr_edges;
+
+    Graph() : Graph(0) {}
+    Graph(const int n) : n(n), m(0), is_build(false), start(n + 1, 0), is_directed(directed) {}
+
+    // 辺を追加し, その辺が何番目に追加されたかを返す
+    int add_edge(const int from, const int to, const T cost = T(1), int id = -1) {
+        assert(!is_build);
+        assert(0 <= from and from < n);
+        assert(0 <= to and to < n);
+        if (id == -1) id = m;
+        edges.emplace_back(from, to, cost, id);
+        return m++;
+    }
+
+    // CSR 形式でグラフを構築
+    void build() {
+        assert(!is_build);
+        for (auto&& e : edges) {
+            start[e.from + 1]++;
+            if (!directed) start[e.to + 1]++;
+        }
+        for (int v = 0; v < n; v++) start[v + 1] += start[v];
+        auto counter = start;
+        csr_edges.resize(start.back() + 1);
+        for (auto&& e : edges) {
+            csr_edges[counter[e.from]++] = e;
+            if (!directed) csr_edges[counter[e.to]++] = Edge(e.to, e.from, e.cost, e.id);
+        }
+        is_build = true;
+    }
+
+    EdgeIterators operator[](int i) {
+        if (!is_build) build();
+        return EdgeIterators(csr_edges.begin() + start[i], csr_edges.begin() + start[i + 1]);
+    }
+
+    size_t size() const { return (size_t)(n); }
+
+    friend std::ostream& operator<<(std::ostream& os, Graph<T, directed>& g) {
+        // output format: {id: cost(from, to) = cost}
+        os << "[";
+        for (int i = 0; i < g.size(); i++) {
+            os << "[";
+            for (int j = 0; j < g[i].size(); j++) {
+                os << g[i][j];
+                if (j + 1 != g[i].size()) os << ", ";
+            }
+            os << "]";
+            if (i + 1 != g.size()) os << ", ";
+        }
+        return os << "]";
+    }
+};
--- graph/read_graph.hpp
+++ graph/read_graph.hpp
@@ -3,41 +3,29 @@
 #include "graph/graph_template.hpp"
 
 template <class T> Graph<T> read_graph(const int n, const int m, const bool weight = false, const bool directed = false, const int offset = 1) {
-    Graph<T> g(n);
+    Graph<T, directed> g(n);
     for (int i = 0; i < m; i++) {
         int a, b;
         std::cin >> a >> b;
         a -= offset, b -= offset;
-        if (weight) {
-            T c;
-            std::cin >> c;
-            if (!directed) g[b].push_back(Edge(b, a, c, i));
-            g[a].push_back(Edge(a, b, c, i));
-        } else {
-            // c = 1
-            if (!directed) g[b].push_back(Edge(b, a, T(1), i));
-            g[a].push_back(Edge(a, b, T(1), i));
-        }
+        T c = 1;
+        if (weight) std::cin >> c;
+        g.add_edge(a, b, c);
     }
+    g.build();
     return g;
 }
 
 template <class T> Graph<T> read_parent(const int n, const bool weight = false, const bool directed = false, const int offset = 1) {
-    Graph<T> g(n);
+    Graph<T, directed> g(n);
     for (int i = 1; i < n; i++) {
         int p;
         std::cin >> p;
         p -= offset;
-        if (weight) {
-            T c;
-            std::cin >> c;
-            if (!directed) g[i].push_back(Edge(i, p, c, i - 1));
-            g[p].push_back(Edge(p, i, c, i - 1));
-        } else {
-            // c = 1
-            if (!directed) g[i].push_back(Edge(i, p, T(1), i - 1));
-            g[p].push_back(Edge(p, i, T(1), i - 1));
-        }
+        T c = 1;
+        if (weight) std::cin >> c;
+        g.add_edge(p, i, c);
     }
+    g.build();
     return g;
 }
