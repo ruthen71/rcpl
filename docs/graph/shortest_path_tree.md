9240ca39fd612b8f977cc9aafe5df59a29b0c620
ruthen71 <takahero2016@gmail.com>
2024-07-29 01:38:49 +0900

    update shortest path tree

--- docs/graph/shortest_path_tree.md
+++ docs/graph/shortest_path_tree.md
@@ -3,8 +3,16 @@ title: Shortest Path Tree (最短経路木)
 documentation_of: //graph/shortest_path_tree.hpp
 ---
 
-- 01-BFSやDijkstraやBellman-Fordでparを求めた後に使う
-- 辺番号のリストを返す
+最短経路木 (有向グラフ) を返す
+
+## 使い方
+
+```cpp
+Graph<int> g(n);
+vector<int> s;
+auto [d, p, r] = dijkstra(g, s, inf);   // zero_one_bfs, bellman_ford でも OK
+auto sptree = shortest_path_tree(g, p); // 最短経路木の 1 つが得られる
+```
 
 ## 使用例
-- [ABC252 F](https://atcoder.jp/contests/abc252/submissions/46239094)
+- [ABC252 E](https://atcoder.jp/contests/abc252/submissions/56095660)
--- graph/shortest_path_tree.hpp
+++ graph/shortest_path_tree.hpp
@@ -1,23 +1,30 @@
 #pragma once
 
 #include <vector>
+#include <cassert>
+#include "graph/graph_template.hpp"
 
 // https://atcoder.jp/contests/abc252/tasks/abc252_e
-template <class T> std::vector<int> shortest_path_tree(Graph<T>& g, std::vector<int>& par) {
+// 最短経路木のうちの 1 つを返す
+template <class T> Graph<T> shortest_path_tree(Graph<T>& g, std::vector<int>& par) {
     const int n = (int)(g.size());
-    std::map<std::pair<int, int>, int> mp;
-    for (int i = 0; i < n; i++) {
-        for (auto& e : g[i]) {
-            mp[{e.from, e.to}] = e.id;
+
+    // 逆向きの辺を貼ったグラフ
+    Graph<T> rg(n, true);
+    for (int v = 0; v < n; v++) {
+        for (auto&& e : g[v]) {
+            rg.add_edge(e.to, e.from, e.cost, e.id);
         }
     }
-    std::vector<int> res;
-    res.reserve(n - 1);
-    for (int i = 0; i < n; i++) {
-        if (par[i] != -1) {
-            res.push_back(mp[{par[i], i}]);
+
+    Graph<T> sptree(n, true);
+    for (int v = 0; v < n; v++) {
+        if (par[v] == -1) continue;
+        for (auto&& e : rg[v]) {
+            if (e.to == par[v]) {
+                sptree.add_edge(e.to, e.from, e.cost, e.id);
+            }
         }
     }
-    assert(int(res.size()) == n - 1);
-    return res;
+    return sptree;
 }
