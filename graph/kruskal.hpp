92ffa4bd9f1aa7ca99b55b7298a90df5d1611a99
ruthen71 <takahero2016@gmail.com>
2024-07-28 21:19:48 +0900

    update kruskal

--- graph/kruskal.hpp
+++ graph/kruskal.hpp
@@ -1,18 +1,17 @@
 #pragma once
 
 #include "data_structure/unionfind.hpp"
-#include "graph/get_edges.hpp"
 #include "graph/graph_template.hpp"
 
-template <class T> std::pair<T, Edges<T>> kruskal(Graph<T> &G) {
-    auto es = get_edges<T>(G);
-    std::sort(es.begin(), es.end(), [](Edge<T> a, Edge<T> b) { return a.cost < b.cost; });
-    int N = (int)G.size();
-    UnionFind uf(N);
+template <class T> std::pair<T, Edges<T>> kruskal(Graph<T>& g) {
+    auto es = g.edges;
+    std::sort(es.begin(), es.end(), [](Edge<T>& a, Edge<T>& b) { return a.cost < b.cost; });
+    const int n = (int)(g.size());
+    UnionFind uf(n);
     T ret = 0;
     Edges<T> es_ret;
-    es_ret.reserve(N - 1);
-    for (auto &&e : es) {
+    es_ret.reserve(n - 1);
+    for (auto&& e : es) {
         if (!uf.same(e.from, e.to)) {
             ret += e.cost;
             uf.merge(e.from, e.to);
