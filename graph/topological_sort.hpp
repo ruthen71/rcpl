5cfda1ccddaff1624d2f09fed376f4d059ec8ee3
ruthen71 <takahero2016@gmail.com>
2024-07-31 19:21:04 +0900

    add atcoder test

--- graph/topological_sort.hpp
+++ graph/topological_sort.hpp
@@ -3,7 +3,11 @@
 #include "graph/graph_template.hpp"
 
 #include <cassert>
+#include <queue>
 
+// topological sort が存在するなら, 辞書順最小のものを返す
+// O((N + M)log N)
+// topological sort が一意に定まる <=> 最長パスの長さが n - 1
 template <class T> std::vector<int> topological_sort(Graph<T>& g) {
     const int n = (int)(g.size());
     assert(n > 0);
@@ -11,18 +15,24 @@ template <class T> std::vector<int> topological_sort(Graph<T>& g) {
     for (int i = 0; i < n; i++) {
         for (auto&& e : g[i]) indeg[e.to]++;
     }
-    std::vector<int> res;
+    // O(N + M) にしたい場合は std::queue にする
+    std::priority_queue<int, std::vector<int>, std::greater<>> que;
     for (int i = 0; i < n; i++) {
-        if (indeg[i] == 0) res.push_back(i);
-    }
-    for (int i = 0; i < (int)(res.size()); i++) {
-        int v = res[i];
-        for (auto&& e : g[v]) {
-            indeg[e.to]--;
-            if (indeg[e.to] == 0) res.push_back(e.to);
+        if (indeg[i] == 0) {
+            que.push(i);
         }
     }
-    // topological sort が一意に定まる <=> 最長パスの長さが n - 1
+    std::vector<int> res;
+    while (!que.empty()) {
+        auto v = que.top();
+        que.pop();
+        res.push_back(v);
+        for (auto&& e : g[v]) {
+            indeg[e.to]--;
+            if (indeg[e.to] == 0) que.push(e.to);
+        }
+    }
+    // topological sort できない
     if ((int)(res.size()) != n) {
         return std::vector<int>();
     }
--- verify/graph/topological_sort.test.cpp
+++ verify/graph/topological_sort.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_4_B"
+#define PROBLEM "https://atcoder.jp/contests/abc223/tasks/abc223_d"
 
 #include <iostream>
 
@@ -8,8 +8,12 @@
 int main() {
     int N, M;
     std::cin >> N >> M;
-    auto g = read_graph<int>(N, M, false, true, 0);
+    auto g = read_graph<int>(N, M, false, true, 1);
     auto vec = topological_sort(g);
-    for (auto&& e : vec) std::cout << e << '\n';
+    if ((int)(vec.size()) != N) {
+        std::cout << -1 << '\n';
+    } else {
+        for (int i = 0; i < N; i++) std::cout << vec[i] + 1 << " \n"[i + 1 == N];
+    }
     return 0;
 }
