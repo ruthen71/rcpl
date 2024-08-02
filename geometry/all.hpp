d7cedad92bed7b2841c52ea05b46580d8ff1db7a
ruthen71 <takahero2016@gmail.com>
2024-08-03 00:38:06 +0900

    add docs

--- geometry/all.hpp
+++ geometry/all.hpp
@@ -3,9 +3,11 @@
 #include "geometry/point.hpp"
 #include "geometry/line.hpp"
 #include "geometry/circle.hpp"
+
 #include "geometry/is_intersect.hpp"
 #include "geometry/cross_point.hpp"
 #include "geometry/distance.hpp"
+
 #include "geometry/tangent_point.hpp"
 #include "geometry/incircle.hpp"
 #include "geometry/circumscribed_circle.hpp"
@@ -14,7 +16,6 @@
 #include "geometry/polygon_contain.hpp"
 #include "geometry/convex_hull_monotone_chain.hpp"
 #include "geometry/convex_polygon_diameter.hpp"
-
 #include "geometry/convex_polygon_cut.hpp"
 
 #include "geometry/closest_pair.hpp"
