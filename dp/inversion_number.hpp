3fc76244401a9095cee2615cd480729d2d260844
ruthen71 <takahero2016@gmail.com>
2025-01-24 21:13:31 +0900

    fix variable names

--- dp/inversion_number.hpp
+++ dp/inversion_number.hpp
@@ -3,16 +3,16 @@
 #include "data_structure/fenwick_tree.hpp"
 #include <algorithm>
 
-template <class T> long long inversion_number(std::vector<T>& A) {
-    auto B = A;
-    sort(B.begin(), B.end());
-    B.erase(unique(B.begin(), B.end()), B.end());
-    int N = (int)B.size();
-    FenwickTree<int> fen(N);
+template <class T> long long inversion_number(std::vector<T>& a) {
+    auto z = a;
+    std::sort(z.begin(), z.end());
+    z.erase(std::unique(z.begin(), z.end()), z.end());
+    const int n = (int)(z.size());
+    FenwickTree<int> fen(n);
     long long ret = 0;
-    for (auto& ai : A) {
-        int i = lower_bound(B.begin(), B.end(), ai) - B.begin();
-        ret += fen.sum(i + 1, N);
+    for (auto& ai : a) {
+        int i = lower_bound(z.begin(), z.end(), ai) - z.begin();
+        ret += fen.sum(i + 1, n);
         fen.add(i, 1);
     }
     return ret;
