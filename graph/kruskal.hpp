7926306c5bcbca49f22f1d8cdf13a5a91cd29246
ruthen71 <takahero2016@gmail.com>
2025-12-10 04:11:40 +0900

    fix: unionfind を使っているライブラリのインクルードパスを修正

--- graph/kruskal.hpp
+++ graph/kruskal.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "data_structure/unionfind.hpp"
+#include "unionfind/unionfind.hpp"
 #include "graph/graph_template.hpp"
 
 #include <algorithm>
@@ -9,7 +9,7 @@ template <class T> std::pair<T, std::vector<Edge<T>>> kruskal(Graph<T>& g) {
     auto es = g.edges;
     std::sort(es.begin(), es.end());
     const int n = (int)(g.size());
-    UnionFind uf(n);
+    Unionfind uf(n);
     T ret = 0;
     std::vector<Edge<T>> es_ret;
     es_ret.reserve(n - 1);
