641de4b07fc12d12045c09f4eea676fbe8e15e80
ruthen71 <takahero2016@gmail.com>
2026-04-10 16:12:00 +0900

    feat: 区間 AND と区間 OR を追加

---
+++ algebra/monoid/monoid_and.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+template <class T> struct MonoidAnd {
+    using value_type = T;
+    static constexpr T operation(const T& a, const T& b) noexcept {
+        return a & b;
+    }
+    static constexpr T identity() noexcept { return T(-1); }
+    static constexpr bool commutative = true;
+};
