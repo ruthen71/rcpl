a1f169d7319556de6bf64e004cce91df495913a6
ruthen71 <takahero2016@gmail.com>
2026-04-10 15:59:59 +0900

    test: 区間最小値のテスト更新

--- verify/lc_data_structure/lc_static_rmq_segment_tree.test.cpp
+++ verify/lc_data_structure/lc_static_rmq_segment_tree.test.cpp
@@ -1,20 +1,27 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <limits>
 
-#include "algebra/monoid_s/monoid_min.hpp"
-#include "data_structure/segment_tree.hpp"
+#include "../../algebra/monoid/monoid_max.hpp"
+#include "../../algebra/monoid/monoid_min.hpp"
+#include "../../data_structure/segment_tree.hpp"
 
 int main() {
     int N, Q;
     std::cin >> N >> Q;
     std::vector<int> A(N);
     for (int i = 0; i < N; i++) std::cin >> A[i];
-    SegmentTree<MonoidMin<int>> seg(A);
+    std::vector<int> B(N);
+    for (int i = 0; i < N; i++) B[i] = -A[i];
+    constexpr int INF = std::numeric_limits<int>::max();
+    SegmentTree<MonoidMin<int, INF>> segmin(A);
+    SegmentTree<MonoidMax<int, INF>> segmax(B);
     while (Q--) {
         int l, r;
         std::cin >> l >> r;
-        std::cout << seg.prod(l, r) << '\n';
+        assert(segmin.prod(l, r) == -segmax.prod(l, r));
+        std::cout << segmin.prod(l, r) << '\n';
     }
     return 0;
 }
