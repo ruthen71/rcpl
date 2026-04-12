135e57c16f3eba452c6c478ec8da165e8e3e8868
ruthen71 <takahero2016@gmail.com>
2026-04-12 15:53:06 +0900

    feat: merge が新ノードを作るケースと更新するケースに対応し update を共通化

--- segment_tree/segment_tree_recursion.hpp
+++ segment_tree/segment_tree_recursion.hpp
@@ -24,24 +24,34 @@ template <class MS> struct SegmentTreeRecursion {
         root = build(v, 0, n);
     }
 
-    Node* merge(Node* l, Node* r) {
-        return new Node(MS::operation(l->d, r->d), l, r);
+    Node* merge(Node* l, Node* r, Node* np = nullptr) {
+        if (np == nullptr) {
+            np = new Node(MS::operation(l->d, r->d), l, r);
+        } else {
+            np->d = MS::operation(l->d, r->d);
+        }
+        return np;
     }
 
     Node* build(const std::vector<S>& v, int l, int r) {
-        if (l + 1 == r) return new Node(v[l]);
-        return merge(build(v, l, (l + r) / 2), build(v, (l + r) / 2, r));
+        if (l + 1 == r) {
+            return new Node(v[l]);
+        }
+        int m = (l + r) / 2;
+        return merge(build(v, l, m), build(v, m, r));
     }
 
-    void update(int qp, const S& qx, int nl, int nr, Node* np) {
-        if (nl + 1 == nr) {
-            np->d = qx;
-            return;
+    Node* update(int p, const S& x, int l, int r, Node* np) {
+        if (l + 1 == r) {
+            np->d = x;
+            return np;
         }
-        int nm = (nl + nr) / 2;
-        if (nl <= qp and qp < nm) update(qp, qx, nl, nm, np->l);
-        if (nm <= qp and qp < nr) update(qp, qx, nm, nr, np->r);
-        np->d = MS::operation(np->l->d, np->r->d);
+        int m = (l + r) / 2;
+        if (l <= p and p < m) {
+            return merge(update(p, x, l, m, np->l), np->r, np);
+        } else {
+            return merge(np->l, update(p, x, m, r, np->r), np);
+        }
     }
 
     void set(int p, const S& x) {
@@ -61,14 +71,14 @@ template <class MS> struct SegmentTreeRecursion {
 
     S prod(int l, int r) const { return inner_prod(l, r, 0, n, root); }
 
-    S inner_prod(int ql, int qr, int nl, int nr, Node* np) const {
-        // [ql, qr) と [nl, nr) が交差しない
-        if (qr <= nl or nr <= ql) return MS::identity();
-        // [ql, qr) が [nl, nr) を完全に含んでいる
-        if (ql <= nl and nr <= qr) return np->d;
-        int nm = (nl + nr) / 2;
-        return MS::operation(inner_prod(ql, qr, nl, nm, np->l),
-                             inner_prod(ql, qr, nm, nr, np->r));
+    S inner_prod(int ql, int qr, int l, int r, Node* np) const {
+        // [ql, qr) と [l, r) が交差しない
+        if (qr <= l or r <= ql) return MS::identity();
+        // [ql, qr) が [l, r) を完全に含んでいる
+        if (ql <= l and r <= qr) return np->d;
+        int m = (l + r) / 2;
+        return MS::operation(inner_prod(ql, qr, l, m, np->l),
+                             inner_prod(ql, qr, m, r, np->r));
     }
 
     S all_prod() const { return root->d; }
